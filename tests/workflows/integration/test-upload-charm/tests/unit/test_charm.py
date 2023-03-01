a78c461e3603dd9159e1958d2e5d33ea07aa3a19
Weii Wang <weii.wang@canonical.com>
2023-03-01 18:09:37 +0800

    Add a test charm for testing publish action

---
+++ tests/workflows/integration/test-upload-charm/tests/unit/test_charm.py
@@ -0,0 +1,14 @@
+# Copyright 2023 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""Unit tests."""
+
+import unittest
+
+
+class TestCharm(unittest.TestCase):
+    """Unit tests."""
+
+    def test(self):
+        """Test case."""
+        pass
