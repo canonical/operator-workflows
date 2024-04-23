980d66a3d74a39ed62fc43bc527f82c235125ae8
Weii Wang <weii.wang@canonical.com>
2024-04-23 11:38:26 +0000

    update

---
+++ src/plan-integration.ts
@@ -0,0 +1,61 @@
+import * as core from '@actions/core'
+import { Plan } from './model'
+import { DefaultArtifactClient } from '@actions/artifact'
+import fs from 'fs'
+import path from 'path'
+import os from 'os'
+import * as exec from '@actions/exec'
+
+export async function run(): Promise<void> {
+  try {
+    const plan: Plan = JSON.parse(core.getInput('plan'))
+    const artifact = new DefaultArtifactClient()
+    let args: string[] = []
+    for (const build of plan.build) {
+      const tmp = fs.mkdtempSync(path.join(os.tmpdir(), 'artifact-'))
+      await artifact.downloadArtifact(
+        (await artifact.getArtifact(build.output)).artifact.id,
+        { path: tmp }
+      )
+      const manifest = JSON.parse(
+        fs.readFileSync(path.join(tmp, 'manifest.json'), { encoding: 'utf-8' })
+      ) as object
+      if (build.type === 'charm') {
+        // @ts-ignore
+        for (const file of manifest.files) {
+          fs.renameSync(path.join(tmp, file), file)
+          args.push(`--charm-file=./${file}`)
+        }
+      }
+      if (build.type === 'rock' || build.type == 'docker-image') {
+        // @ts-ignore
+        const name = manifest.name as string
+        if ('files' in manifest) {
+          for (const file of manifest.files as string[]) {
+            const image = `localhost:32000/${name}:${file.replace(`${name}_`, '')}`
+            await exec.exec('skopeo', [
+              'copy',
+              '--insecure-policy',
+              '--dest-tls-verify=false',
+              `oci-archive:${path.join(tmp, file)}`,
+              `docker://${image}`
+            ])
+            args.push(`--${name}-image=${image}`)
+          }
+        }
+        if ('images' in manifest) {
+          for (const image of manifest.images as string[]) {
+            args.push(`--${name}-image=${image}`)
+          }
+        }
+      }
+    }
+    core.setOutput('args', args.join(' '))
+  } catch (error) {
+    // Fail the workflow run if an error occurs
+    if (error instanceof Error) core.setFailed(error.message)
+  }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+run()
