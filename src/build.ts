b3db96cc8bfeac162e794bf29fcf14e7b7087793
Weii Wang <weii.wang@canonical.com>
2024-05-06 12:00:11 +0000

    Better build and publish pipeline (#285)

---
+++ src/build.ts
@@ -0,0 +1,377 @@
+// Copyright 2024 Canonical Ltd.
+// See LICENSE file for licensing details.
+
+import * as core from '@actions/core'
+import * as exec from '@actions/exec'
+import * as glob from '@actions/glob'
+import * as cache from '@actions/cache'
+import * as github from '@actions/github'
+
+import { BuildPlan } from './model'
+import { DefaultArtifactClient } from '@actions/artifact'
+import fs from 'fs'
+import path from 'path'
+import os from 'os'
+
+async function installSnapcraft(): Promise<void> {
+  const snapcraftInfo = (
+    await exec.getExecOutput('snap', ['info', 'snapcraft'])
+  ).stdout
+  if (snapcraftInfo.includes('installed')) {
+    return
+  }
+  await exec.exec('sudo', ['snap', 'install', 'snapcraft', '--classic'])
+}
+
+async function buildInstallCharmcraft(
+  repository: string,
+  ref: string
+): Promise<void> {
+  const workingDir = '/opt/operator-workflows/charmcraft'
+  await exec.exec('sudo', ['mkdir', workingDir, '-p'])
+  await exec.exec('sudo', ['chown', os.userInfo().username, workingDir])
+  await exec.exec('git', [
+    'clone',
+    `https://github.com/${repository}.git`,
+    '--branch',
+    ref,
+    workingDir
+  ])
+  const charmcraftSha = (
+    await exec.getExecOutput('git', ['rev-parse', 'HEAD'], { cwd: workingDir })
+  ).stdout.trim()
+  const cacheKey = `charmcraft-${charmcraftSha}`
+  const charmcraftGlob = path.join(workingDir, 'charmcraft*.snap')
+  const restored = await cache.restoreCache([charmcraftGlob], cacheKey)
+  if (!restored) {
+    await installSnapcraft()
+    core.startGroup('snapcraft pack (charmcraft)')
+    await exec.exec('snapcraft', ['--use-lxd', '--verbosity', 'trace'], {
+      cwd: workingDir
+    })
+    core.endGroup()
+  }
+  const charmcraftSnaps = await (await glob.create(charmcraftGlob)).glob()
+  if (charmcraftSnaps.length == 0) {
+    throw new Error('can\'t find charmcraft snap')
+  }
+  await exec.exec('sudo', [
+    'snap',
+    'install',
+    charmcraftSnaps[0],
+    '--classic',
+    '--dangerous'
+  ])
+  if (!restored) {
+    await cache.saveCache([charmcraftGlob], cacheKey)
+  }
+}
+
+interface BuildCharmParams {
+  plan: BuildPlan
+  charmcraftChannel: string
+  charmcraftRepository: string
+  charmcraftRef: string
+}
+
+async function gitTreeId(p: string): Promise<string> {
+  const gitPath = path.resolve(p) == path.resolve(process.cwd()) ? '' : p
+  const tree = (
+    await exec.getExecOutput('git', ['rev-parse', `HEAD:${gitPath}`])
+  ).stdout.trim()
+  if (!tree) {
+    throw new Error(`failed to get git tree id for path: ${p}`)
+  }
+  return tree
+}
+
+async function buildCharm(params: BuildCharmParams): Promise<void> {
+  if (params.charmcraftRepository && params.charmcraftRef) {
+    await buildInstallCharmcraft(
+      params.charmcraftRepository,
+      params.charmcraftRef
+    )
+  } else if (params.charmcraftChannel) {
+    await exec.exec('sudo', [
+      'snap',
+      'install',
+      'charmcraft',
+      '--channel',
+      params.charmcraftChannel,
+      '--classic'
+    ])
+  } else {
+    await exec.exec('sudo', ['snap', 'install', 'charmcraft', '--classic'])
+  }
+  core.startGroup('charmcraft pack')
+  await exec.exec('charmcraft', ['pack', '--verbosity', 'trace'], {
+    cwd: params.plan.source_directory,
+    env: { CHARMCRAFT_ENABLE_EXPERIMENTAL_EXTENSIONS: 'true' }
+  })
+  core.endGroup()
+  const charmFiles = await (
+    await glob.create(path.join(params.plan.source_directory, '*.charm'))
+  ).glob()
+  const artifact = new DefaultArtifactClient()
+  const manifestFile = path.join(params.plan.source_directory, 'manifest.json')
+  fs.writeFileSync(
+    manifestFile,
+    JSON.stringify(
+      { name: params.plan.name, files: charmFiles.map(f => path.basename(f)) },
+      null,
+      2
+    )
+  )
+  await artifact.uploadArtifact(
+    params.plan.output,
+    [...charmFiles, manifestFile],
+    params.plan.source_directory
+  )
+}
+
+interface BuildDockerImageParams {
+  plan: BuildPlan
+  user: string
+  token: string
+}
+
+async function buildDockerImage({
+                                  plan,
+                                  user,
+                                  token
+                                }: BuildDockerImageParams): Promise<void> {
+  const tag = await gitTreeId(plan.source_directory)
+  const imageName = `${plan.name}:${tag}`
+  await exec.exec(
+    'docker',
+    [
+      'build',
+      '-t',
+      imageName,
+      '-f',
+      path.relative(plan.source_directory, plan.source_file),
+      '.'
+    ],
+    { cwd: plan.source_directory }
+  )
+  const artifact = new DefaultArtifactClient()
+  const manifest = path.join(plan.source_directory, 'manifest.json')
+  if (plan.output_type == 'file') {
+    const file = `${plan.name}-${tag}.tar`
+    fs.writeFileSync(
+      manifest,
+      JSON.stringify({ name: plan.name, files: [file] }, null, 2)
+    )
+    await exec.exec('docker', ['save', '-o', file, imageName], {
+      cwd: plan.source_directory
+    })
+    await artifact.uploadArtifact(
+      plan.output,
+      [manifest, path.join(plan.source_directory, file)],
+      plan.source_directory
+    )
+  }
+  if (plan.output_type == 'registry') {
+    await exec.exec(
+      `docker`,
+      ['login', '-u', user, '--password-stdin', 'ghcr.io'],
+      { input: Buffer.from(`${token}\n`, 'utf-8') }
+    )
+    const registryImageName = `ghcr.io/${github.context.repo.owner}/${imageName}`
+    await exec.exec(`docker`, ['image', 'tag', imageName, registryImageName])
+    await exec.exec('docker', ['push', registryImageName])
+    fs.writeFileSync(
+      manifest,
+      JSON.stringify({ name: plan.name, images: [registryImageName] }, null, 2)
+    )
+    await artifact.uploadArtifact(
+      plan.output,
+      [manifest],
+      plan.source_directory
+    )
+  }
+}
+
+async function buildInstallRockcraft(
+  repository: string,
+  ref: string
+): Promise<void> {
+  const workingDir = '/opt/operator-workflows/rockcraft'
+  await exec.exec('sudo', ['mkdir', workingDir, '-p'])
+  await exec.exec('sudo', ['chown', os.userInfo().username, workingDir])
+  await exec.exec('git', [
+    'clone',
+    `https://github.com/${repository}.git`,
+    '--branch',
+    ref,
+    workingDir
+  ])
+  const rockcraftSha = (
+    await exec.getExecOutput('git', ['rev-parse', 'HEAD'], { cwd: workingDir })
+  ).stdout.trim()
+  const cacheKey = `rockcraft-${rockcraftSha}`
+  const rockcraftGlob = path.join(workingDir, 'rockcraft*.snap')
+  const restored = await cache.restoreCache([rockcraftGlob], cacheKey)
+  if (!restored) {
+    await installSnapcraft()
+    core.startGroup('snapcraft pack (rockcraft)')
+    await exec.exec('snapcraft', ['--use-lxd', '--verbosity', 'trace'], {
+      cwd: workingDir
+    })
+    core.endGroup()
+  }
+  const rockcraftSnaps = await (await glob.create(rockcraftGlob)).glob()
+  if (rockcraftSnaps.length == 0) {
+    throw new Error('can\'t find rockcraft snap')
+  }
+  await exec.exec('sudo', [
+    'snap',
+    'install',
+    rockcraftSnaps[0],
+    '--classic',
+    '--dangerous'
+  ])
+  if (!restored) {
+    await cache.saveCache([rockcraftGlob], cacheKey)
+  }
+}
+
+interface BuildRockParams {
+  plan: BuildPlan
+  rockcraftChannel: string
+  rockcraftRepository: string
+  rockcraftRef: string
+  user: string
+  token: string
+}
+
+async function buildRock({
+                           plan,
+                           rockcraftChannel,
+                           rockcraftRepository,
+                           rockcraftRef,
+                           user,
+                           token
+                         }: BuildRockParams): Promise<void> {
+  if (rockcraftRepository && rockcraftRef) {
+    await buildInstallRockcraft(rockcraftRepository, rockcraftRef)
+  } else if (rockcraftChannel) {
+    await exec.exec('sudo', [
+      'snap',
+      'install',
+      'rockcraft',
+      '--channel',
+      rockcraftChannel,
+      '--classic'
+    ])
+  } else {
+    await exec.exec('sudo', ['snap', 'install', 'rockcraft', '--classic'])
+  }
+  core.startGroup('rockcraft pack')
+  await exec.exec('rockcraft', ['pack', '--verbosity', 'trace'], {
+    cwd: plan.source_directory,
+    env: { ROCKCRAFT_ENABLE_EXPERIMENTAL_EXTENSIONS: 'true' }
+  })
+  core.endGroup()
+  const rocks = await (
+    await glob.create(path.join(plan.source_directory, '*.rock'))
+  ).glob()
+  const manifestFile = path.join(plan.source_directory, 'manifest.json')
+  const artifact = new DefaultArtifactClient()
+  if (plan.output_type === 'file') {
+    fs.writeFileSync(
+      manifestFile,
+      JSON.stringify(
+        {
+          name: plan.name,
+          files: rocks.map(f => path.basename(f))
+        },
+        null,
+        2
+      )
+    )
+    await artifact.uploadArtifact(
+      plan.output,
+      [...rocks, manifestFile],
+      plan.source_directory
+    )
+  } else {
+    const tree = await gitTreeId(plan.source_directory)
+    const images = await Promise.all(
+      rocks.map(async f => {
+        const base = path
+          .basename(f)
+          .substring(plan.name.length)
+          .replace(/\.rock$/, '')
+        const image = `ghcr.io/${github.context.repo.owner}/${plan.name}:${tree}-${base}`
+        await exec.exec(
+          '/snap/rockcraft/current/bin/skopeo',
+          [
+            '--insecure-policy',
+            'copy',
+            `oci-archive:${path.basename(f)}`,
+            `docker://${image}`,
+            '--dest-creds',
+            `${user}:${token}`
+          ],
+          { cwd: plan.source_directory }
+        )
+        return image
+      })
+    )
+    fs.writeFileSync(
+      manifestFile,
+      JSON.stringify(
+        {
+          name: plan.name,
+          images: images
+        },
+        null,
+        2
+      )
+    )
+    await artifact.uploadArtifact(
+      plan.output,
+      [manifestFile],
+      plan.source_directory
+    )
+  }
+}
+
+export async function run(): Promise<void> {
+  try {
+    const plan: BuildPlan = JSON.parse(core.getInput('build-plan'))
+    switch (plan.type) {
+      case 'charm':
+        await buildCharm({
+          plan,
+          charmcraftChannel: core.getInput('charmcraft-channel'),
+          charmcraftRef: core.getInput('charmcraft-ref'),
+          charmcraftRepository: core.getInput('charmcraft-repository')
+        })
+        break
+      case 'docker-image':
+        await buildDockerImage({
+          plan,
+          user: github.context.actor,
+          token: core.getInput('github-token')
+        })
+        break
+      case 'rock':
+        await buildRock({
+          plan,
+          rockcraftChannel: core.getInput('rockcraft-channel'),
+          rockcraftRef: core.getInput('rockcraft-ref'),
+          rockcraftRepository: core.getInput('rockcraft-repository'),
+          user: github.context.actor,
+          token: core.getInput('github-token')
+        })
+    }
+  } catch (error) {
+    // Fail the workflow run if an error occurs
+    if (error instanceof Error) core.setFailed(error.message)
+  }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+run()
---
+++ src/publish.ts
@@ -0,0 +1,326 @@
+// Copyright 2024 Canonical Ltd.
+// See LICENSE file for licensing details.
+
+import * as core from '@actions/core'
+import * as exec from '@actions/exec'
+import * as yaml from 'js-yaml'
+
+import * as github from '@actions/github'
+import { Plan } from './model'
+import { DefaultArtifactClient } from '@actions/artifact'
+import fs from 'fs'
+import path from 'path'
+import os from 'os'
+
+class Publish {
+  private token: string
+  private charmhubToken: string
+  private octokit
+  private workflowFile: string
+  private artifact
+  private workingDir: string
+  private resourceMapping: { [key: string]: string }
+  private workflowRunId: string
+  private identifier: string
+
+  constructor() {
+    this.token = core.getInput('github-token')
+    this.charmhubToken = core.getInput('charmhub-token')
+    this.octokit = github.getOctokit(this.token)
+    this.workflowFile = core.getInput('workflow-file')
+    this.workingDir = core.getInput('working-directory')
+    this.resourceMapping = JSON.parse(core.getInput('resource-mapping'))
+    this.workflowRunId = core.getInput('workflow-run-id')
+    this.identifier = core.getInput('identifier')
+    this.artifact = new DefaultArtifactClient()
+  }
+
+  mkdtemp(): string {
+    return fs.mkdtempSync(path.join(os.tmpdir(), 'artifact-'))
+  }
+
+  async findWorkflowRunId(): Promise<number> {
+    if (this.workflowRunId !== '') {
+      return parseInt(this.workflowRunId)
+    }
+    const owner = github.context.repo.owner
+    const repo = github.context.repo.repo
+    const commit = await this.octokit.rest.git.getCommit({
+      owner,
+      repo,
+      commit_sha: github.context.sha
+    })
+    const tree = commit.data.tree.sha
+    const workflowResp = await this.octokit.rest.actions.getWorkflow({
+      owner,
+      repo,
+      workflow_id: this.workflowFile
+    })
+    if (workflowResp.status !== 200) {
+      throw new Error(
+        `failed to find integration workflow (${this.workflowFile}): status ${workflowResp.status}`
+      )
+    }
+    const runIter = this.octokit.paginate.iterator(
+      this.octokit.rest.actions.listWorkflowRuns,
+      {
+        owner,
+        repo,
+        workflow_id: workflowResp.data.id,
+        status: 'success',
+        created: `>${new Date(Date.now() - 7 * 24 * 60 * 60 * 1000).toISOString().slice(0, 10)}`
+      }
+    )
+    for await (const resp of runIter) {
+      if (resp.status !== 200) {
+        throw new Error(
+          `failed to find integration workflow run: status ${resp.status}`
+        )
+      }
+      for (const run of resp.data) {
+        if (run?.head_commit?.tree_id === tree) {
+          return run.id
+        }
+      }
+    }
+    throw new Error(
+      `Failed to find integration test workflow run on tree id (${tree}).` +
+        "Consider enabling the 'Require branches to be up to date before merging' setting to ensure that the integration tests are executed on the merged commit"
+    )
+  }
+
+  async getPlan(runId: number): Promise<Plan> {
+    const artifacts = (
+      await this.octokit.paginate(
+        this.octokit.rest.actions.listWorkflowRunArtifacts,
+        {
+          owner: github.context.repo.owner,
+          repo: github.context.repo.repo,
+          run_id: runId
+        }
+      )
+    )
+      .filter(a =>
+        a.name.endsWith(
+          `${this.identifier ? '__' : ''}${this.identifier}__plan`
+        )
+      )
+      .sort()
+    if (artifacts.length === 0) {
+      throw new Error(`can't find plan artifact for workflow run ${runId}`)
+    }
+    const artifact = artifacts[artifacts.length - 1]
+    const tmp = this.mkdtemp()
+    await this.artifact.downloadArtifact(artifact.id, { path: tmp })
+    return JSON.parse(
+      fs.readFileSync(path.join(tmp, 'plan.json'), { encoding: 'utf-8' })
+    ) as Plan
+  }
+
+  async getImageResources(): Promise<string[]> {
+    interface Metadata {
+      resources?: {
+        [name: string]: {
+          type: string
+        }
+      }
+    }
+
+    let metadata = yaml.load(
+      (
+        await exec.getExecOutput('charmcraft', ['expand-extensions'], {
+          cwd: this.workingDir
+        })
+      ).stdout
+    ) as Metadata
+    if (
+      metadata.resources === undefined ||
+      Object.keys(metadata.resources).length === 0
+    ) {
+      metadata = yaml.load(
+        fs.readFileSync(path.join(this.workingDir, 'metadata.yaml'), {
+          encoding: 'utf-8'
+        })
+      ) as Metadata
+    }
+    const resources = metadata.resources
+    if (resources === undefined) {
+      return []
+    }
+    return Object.keys(resources).filter(k => resources[k].type === 'oci-image')
+  }
+
+  async getImages(): Promise<Map<string, string>> {
+    const resources = await this.getImageResources()
+    core.info(`required resources: ${resources}`)
+    const upload: Map<string, string> = new Map()
+    if (resources.length === 0) {
+      return upload
+    }
+    const runId = await this.findWorkflowRunId()
+    const plan = await this.getPlan(runId)
+    for (const build of plan.build) {
+      if (build.type === 'charm') {
+        continue
+      }
+      const resourceName = this.resourceMapping.hasOwnProperty(build.name)
+        ? this.resourceMapping[build.name]
+        : `${build.name}-image`
+      if (!resources.includes(resourceName)) {
+        core.info(`skip uploading image: ${build.name}`)
+        continue
+      }
+      const tmp = this.mkdtemp()
+      const artifact = await this.artifact.getArtifact(build.output, {
+        findBy: {
+          token: this.token,
+          repositoryOwner: github.context.repo.owner,
+          repositoryName: github.context.repo.repo,
+          workflowRunId: runId
+        }
+      })
+      await this.artifact.downloadArtifact(artifact.artifact.id, {
+        path: tmp,
+        findBy: {
+          token: this.token,
+          repositoryOwner: github.context.repo.owner,
+          repositoryName: github.context.repo.repo,
+          workflowRunId: runId
+        }
+      })
+      const manifest = JSON.parse(
+        fs.readFileSync(path.join(tmp, 'manifest.json'), { encoding: 'utf-8' })
+      )
+      if (build.output_type === 'registry') {
+        const images = manifest.images as string[]
+        if (images.length !== 1) {
+          throw new Error(
+            `image resource ${build.name} contain multiple candidates: ${images}`
+          )
+        }
+        const image = images[0]
+        await exec.exec('docker', ['pull', image])
+        upload.set(resourceName, image)
+      }
+      if (build.output_type === 'file') {
+        const files = manifest.files as string[]
+        if (files.length !== 1) {
+          throw new Error(
+            `image resource ${build.name} contain multiple candidates: ${files}`
+          )
+        }
+        const file = files[0]
+        const image = `${build.name}:${file}`
+        const archiveType = file.endsWith('.rock')
+          ? 'oci-archive'
+          : 'docker-archive'
+        await exec.exec('skopeo', [
+          'copy',
+          '--insecure-policy',
+          `${archiveType}:${path.join(tmp, file)}`,
+          `docker-daemon:${image}`
+        ])
+        upload.set(resourceName, image)
+      }
+    }
+    if (upload.size != resources.length) {
+      const missing = resources.filter(r => !upload.has(r))
+      throw new Error(`can't find required resources: ${missing}`)
+    }
+    return upload
+  }
+
+  async getCharms(): Promise<{ name: string; dir: string; files: string[] }> {
+    const runId = await this.findWorkflowRunId()
+    const plan = await this.getPlan(runId)
+    const charms = plan.build.filter(b => b.type === 'charm')
+    if (charms.length === 0) {
+      throw new Error('no charm to upload')
+    }
+    if (charms.length > 1) {
+      throw new Error(
+        `more than one charm to upload: ${charms.map(c => c.name)}`
+      )
+    }
+    const charm = charms[0]
+    const tmp = this.mkdtemp()
+    const artifact = (
+      await this.artifact.getArtifact(charm.output, {
+        findBy: {
+          token: this.token,
+          repositoryOwner: github.context.repo.owner,
+          repositoryName: github.context.repo.repo,
+          workflowRunId: runId
+        }
+      })
+    ).artifact
+    await this.artifact.downloadArtifact(artifact.id, {
+      path: tmp,
+      findBy: {
+        token: this.token,
+        repositoryOwner: github.context.repo.owner,
+        repositoryName: github.context.repo.repo,
+        workflowRunId: runId
+      }
+    })
+    const manifest = JSON.parse(
+      fs.readFileSync(path.join(tmp, 'manifest.json'), { encoding: 'utf-8' })
+    )
+    return {
+      name: manifest.name as string,
+      dir: charm.source_directory,
+      files: (manifest.files as string[]).map(f => path.join(tmp, f))
+    }
+  }
+
+  async run() {
+    try {
+      core.startGroup('retrieve image info')
+      const images = await this.getImages()
+      core.endGroup()
+      core.startGroup('retrieve charm info')
+      const {
+        name: charmName,
+        dir: charmDir,
+        files: charms
+      } = await this.getCharms()
+      core.endGroup()
+      core.info(
+        `start uploading image resources: ${JSON.stringify(Object.fromEntries([...images]))}`
+      )
+      for (const resource of images.keys()) {
+        core.info(`upload resource ${resource}`)
+        const imageId = (
+          await exec.getExecOutput('docker', [
+            'images',
+            images.get(resource) as string,
+            '--format',
+            '{{.ID}}'
+          ])
+        ).stdout.trim()
+        await exec.exec(
+          'charmcraft',
+          [
+            'upload-resource',
+            charmName,
+            resource,
+            `--image=${imageId}`,
+            '--verbosity=brief'
+          ],
+          { env: { CHARMCRAFT_AUTH: this.charmhubToken } }
+        )
+      }
+      core.setOutput('charms', charms.join(','))
+      core.setOutput('charm-directory', charmDir)
+    } catch (error) {
+      // Fail the workflow run if an error occurs
+      if (error instanceof Error) {
+        core.error(`${error.message}\n${error.stack}`)
+        core.setFailed(error.message)
+      }
+    }
+  }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+new Publish().run()
