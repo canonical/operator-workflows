d337dc84addde29f73d78963b9f1e6816adaec3b
Weii Wang <weii.wang@canonical.com>
2025-10-01 12:11:47 +0200

    Allow alternative rockcraft.yaml file names (#837)

---
+++ tests/workflows/integration/test-alter-rock-name/src/charm.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+# Copyright 2024 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""A charm for tesing charm upload."""
+
+from ops.charm import CharmBase
+from ops.main import main
+from ops.model import ActiveStatus
+
+
+class TestUploadCharm(CharmBase):
+    """Charm the service."""
+
+    def __init__(self, *args):
+        """Initialize the charm.
+
+        Args:
+            args: charm initialization arguments.
+        """
+        super().__init__(*args)
+        self.unit.status = ActiveStatus()
+
+
+if __name__ == "__main__":  # pragma: nocover
+    main(TestUploadCharm)
