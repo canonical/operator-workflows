053d5933c37f3ae9ed8d9ba502a23752b4903865
Weii Wang <weii.wang@canonical.com>
2026-02-10 21:28:28 +0800

    Migrate test charms to use uv

--- tests/workflows/integration/test-upload-charm/tests/conftest.py
+++ tests/workflows/integration/test-upload-charm/tests/conftest.py
@@ -5,7 +5,26 @@
 
 
 def pytest_addoption(parser):
-    """Add test arguments."""
+    """Parse additional pytest options.
+
+    Args:
+        parser: Pytest parser.
+    """
     parser.addoption("--charm-file", action="store")
-    parser.addoption("--test-image", action="store")
-    parser.addoption("--test-file-resource", action="store")
+    parser.addoption(
+        "--keep-models",
+        action="store_true",
+        default=False,
+        help="keep temporarily-created models",
+    )
+    parser.addoption(
+        "--use-existing",
+        action="store_true",
+        default=False,
+        help="use existing models and not created models",
+    )
+    parser.addoption(
+        "--model",
+        action="store",
+        help="temporarily-created model name",
+    )
