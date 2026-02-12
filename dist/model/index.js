e26340ffe513b5f467205e40b4a9035879603e95
charlie4284 <yangsoo.yoon@canonical.com>
2026-02-12 06:06:06 +0000

    chore: build dist

--- dist/model/index.js
+++ dist/model/index.js
@@ -1,22 +1,12 @@
-require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
-/******/ 	/* webpack/runtime/compat */
-/******/ 	
-/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
-/******/ 	
+import './sourcemap-register.cjs';/******/ /* webpack/runtime/compat */
+/******/ 
+/******/ if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = new URL('.', import.meta.url).pathname.slice(import.meta.url.match(/^file:\/\/\/\w:/) ? 1 : 0, -1) + "/";
+/******/ 
 /************************************************************************/
 var __webpack_exports__ = {};
-// This entry need to be wrapped in an IIFE because it uses a non-standard name for the exports (exports).
-(() => {
-var exports = __webpack_exports__;
-
 // Copyright 2025 Canonical Ltd.
 // See LICENSE file for licensing details.
-Object.defineProperty(exports, "__esModule", ({ value: true }));
 
-})();
 
-module.exports = __webpack_exports__;
-/******/ })()
-;
+
 //# sourceMappingURL=index.js.map
