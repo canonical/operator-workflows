4a74eb7b1beb1db37e338f21baa93edca17db5e6
charlie4284 <yangsoo.yoon@canonical.com>
2026-03-02 12:01:47 +0800

    chore: replace ncc build tool with rollup

---
+++ rollup.config.ts
@@ -0,0 +1,31 @@
+import commonjs from '@rollup/plugin-commonjs'
+import nodeResolve from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript'
+
+const entryPoints = [
+    'build',
+    'get-plan',
+    'model',
+    'plan-integration',
+    'plan-scan',
+    'plan',
+    'publish',
+    'utils'
+]
+
+const configs = entryPoints.map(entry => ({
+    input: `src/${entry}.ts`,
+    output: {
+        esModule: true,
+        file: `dist/${entry}/index.js`,
+        format: 'es',
+        sourcemap: true
+    },
+    plugins: [
+        typescript(),
+        nodeResolve({ preferBuiltins: true }),
+        commonjs()
+    ]
+}))
+
+export default configs
