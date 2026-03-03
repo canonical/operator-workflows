40597160943925cff3f3b18cb81466c80e895607
charlie4284 <yangsoo.yoon@canonical.com>
2026-03-03 03:16:07 +0000

    ci: add eslint & rollup configs

---
+++ rollup.config.ts
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2026 Canonical Ltd.
+ * See LICENSE file for licensing details.
+ */
+
+import commonjs from '@rollup/plugin-commonjs'
+import nodeResolve from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript'
+import json from '@rollup/plugin-json'
+
+const entryPoints = [
+  'build',
+  'get-plan',
+  'model',
+  'plan-integration',
+  'plan-scan',
+  'plan',
+  'publish',
+  'utils'
+]
+
+const configs = entryPoints.map(entry => ({
+  input: `src/${entry}.ts`,
+  output: {
+    file: `dist/${entry}/index.js`,
+    format: 'cjs',
+    sourcemap: true
+  },
+  plugins: [
+    typescript({
+      compilerOptions: {
+        module: 'ESNext',
+        moduleResolution: 'Bundler'
+      }
+    }),
+    json(),
+    nodeResolve({ preferBuiltins: true }),
+    commonjs()
+  ]
+}))
+
+export default configs
